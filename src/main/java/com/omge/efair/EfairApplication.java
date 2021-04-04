71d37ef73b002ffdb3d8c3f2d06dacc955ac0e6c
zgl <zgl_sh_cn@126.com>
2021-04-04 13:15:07 +0800

    init

---
+++ src/main/java/com/omge/efair/EfairApplication.java
@@ -0,0 +1,13 @@
+package com.omge.efair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfairApplication.class, args);
+    }
+
+}
