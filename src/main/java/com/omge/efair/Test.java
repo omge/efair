f9e40a6502d96cd85d28d0c71b7faeeb12e2034d
zgl <zgl_sh_cn@126.com>
2021-04-04 22:04:17 +0800

    test

--- src/main/java/com/omge/efair/Test.java
+++ src/main/java/com/omge/efair/Test.java
@@ -1,4 +1,16 @@
 package com.omge.efair;
 
 public class Test {
+
+
+    public static void main(String[] args) {
+        //todo:
+
+        Integer a = Integer.valueOf(200);
+        Integer b = Integer.valueOf(200);
+
+
+
+        System.out.println( "res=" + a +"\t" + b +"\t" + (a == b) );
+    }
 }
